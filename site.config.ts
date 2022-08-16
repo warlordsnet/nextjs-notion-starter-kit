1efd58b77cc49a4150e299ba4e571c27c577503a
Warlords Network <96106468+warlordsnet@users.noreply.github.com>
2022-08-16 18:46:05 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,26 +2,30 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '987748811ef9480bad6120117ddfa59d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Kaiziyo: The generic animanga blog.',
+  domain: 'kaiziyo.vercel.app',
+  author: 'Kaiziyo',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Welcome to Kaiziyo: The generic animanga blog, I write about random generic anime shit.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  twitter: '_kaiziyo_',
+  insta: '_kaiziyo_',
+  fb : '#',
+  github: '#',
+  discord: '#',
+  telegram: '3',
+ // linkedin: '#',
+ // newsletter: '#', // optional newsletter URL
+ // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -49,16 +53,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+    {
+      title: 'About',
+       pageId: '2f7c1b62390b4c229ba27755394251fb'
+     },
+     {
+       title: 'Contact',
+       pageId: 'd8e0ff285f4a43aa8cbad0765f4e046f'
+     }
+  ]
 })
